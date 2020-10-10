dc934936300b0870ad96d1597e7bc2f78a2428c6
erikscow <erikscow@gmail.com>
2020-10-09 19:42:44 -0700

    attempted short answer attempted recursive count. Did not finish. Need to flex this unit.

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,13 +2,13 @@
 
 ## Exercise I
 
-a)
+a) O(n)
 
 
-b)
+b) O(n)
 
 
-c)
+c) o(n)
 
 ## Exercise II
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,30 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
 def count_th(word):
+    global count 
+    count = 0
+
+    high = len(word) - 1
+    low = 0
+
+    mid = (high + low) // 2
+
+    right = word[mid:high + 1]
+    left = word[low:mid]
+
+    print(right, left)
     
-    # TBC
-    
-    pass
+    if len(word) == 0:
+        return 0
+    if len(word) == 2:
+        if word == 'th':
+            count = count + 1
+            return count
+    else:
+        count_th(left)
+        count_th(right)
+
+    print(count)
+    return count
